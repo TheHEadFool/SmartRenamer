4a024d6d5ec29e43ed8bc3c2adcbd05bf94155f0
Jonathan R Holman <Theheadfool@gmail.com>
2026-07-09 16:21:05 -0700

    refactor: add RelayCommand infrastructure

--- src/SmartRenamer/MainWindow.xaml.cs
+++ src/SmartRenamer/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using SmartRenamer.Models;
+using System;
+using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartRenamer
 {
@@ -16,9 +10,73 @@ namespace SmartRenamer
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<RenameItem> Files { get; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void AddFiles_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Multiselect = true
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                foreach (string file in dialog.FileNames)
+                {
+                    Files.Add(new RenameItem
+                    {
+                        CurrentName = System.IO.Path.GetFileName(file),
+                        NewName = System.IO.Path.GetFileName(file),
+                        FullPath = file,
+                        Status = "Ready"
+                    });
+                }
+            }
+        }
+
+        private void Preview_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (RenameItem item in Files)
+            {
+                item.NewName = item.CurrentName.Replace(
+                    FindTextBox.Text,
+                    ReplaceTextBox.Text);
+            }
+        }
+
+        private void Rename_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (RenameItem item in Files)
+            {
+                try
+                {
+                    string folder = System.IO.Path.GetDirectoryName(item.FullPath)!;
+
+                    string newPath = System.IO.Path.Combine(folder, item.NewName);
+
+                    if (System.IO.File.Exists(newPath))
+                    {
+                        item.Status = "File already exists";
+                        continue;
+                    }
+
+                    System.IO.File.Move(item.FullPath, newPath);
+
+                    item.FullPath = newPath;
+                    item.CurrentName = item.NewName;
+                    item.Status = "Renamed";
+                }
+                catch (Exception ex)
+                {
+                    item.Status = ex.Message;
+                }
+            }
         }
     }
 }
---
+++ src/SmartRenamer/Models/RenameItem.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace SmartRenamer.Models
+{
+    public class RenameItem : INotifyPropertyChanged
+    {
+        private string currentName = "";
+        private string newName = "";
+        private string status = "";
+        private string fullPath = "";
+
+        public string CurrentName
+        {
+            get => currentName;
+            set
+            {
+                currentName = value;
+                OnPropertyChanged(nameof(CurrentName));
+            }
+        }
+
+        public string NewName
+        {
+            get => newName;
+            set
+            {
+                newName = value;
+                OnPropertyChanged(nameof(NewName));
+            }
+        }
+
+        public string Status
+        {
+            get => status;
+            set
+            {
+                status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        public string FullPath
+        {
+            get => fullPath;
+            set
+            {
+                fullPath = value;
+                OnPropertyChanged(nameof(FullPath));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

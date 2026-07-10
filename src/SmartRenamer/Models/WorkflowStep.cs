dec6e952d84019c06c74285f31c504bde3e1a92e
Jonathan R Holman <Theheadfool@gmail.com>
2026-07-09 18:41:59 -0700

    feat: introduce workflow engine and pipeline architecture

---
+++ src/SmartRenamer/Models/WorkflowStep.cs
@@ -0,0 +1,16 @@
+﻿using SmartRenamer.Interfaces;
+
+namespace SmartRenamer.Models
+{
+    public class WorkflowStep
+    {
+        public IPipelineStep Step { get; }
+
+        public WorkflowStep(IPipelineStep step)
+        {
+            Step = step;
+        }
+
+        public string Name => Step.Name;
+    }
+}
---
+++ src/SmartRenamer/ViewModels/PipelineViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using SmartRenamer.Models;
+
+namespace SmartRenamer.ViewModels
+{
+    public class PipelineViewModel
+    {
+        public ObservableCollection<WorkflowStep> Steps { get; }
+            = new();
+
+        public void AddStep(WorkflowStep step)
+        {
+            Steps.Add(step);
+        }
+
+        public void RemoveStep(WorkflowStep step)
+        {
+            Steps.Remove(step);
+        }
+
+        public void MoveUp(WorkflowStep step)
+        {
+            int index = Steps.IndexOf(step);
+
+            if (index > 0)
+            {
+                Steps.Move(index, index - 1);
+            }
+        }
+
+        public void MoveDown(WorkflowStep step)
+        {
+            int index = Steps.IndexOf(step);
+
+            if (index < Steps.Count - 1)
+            {
+                Steps.Move(index, index + 1);
+            }
+        }
+    }
+}

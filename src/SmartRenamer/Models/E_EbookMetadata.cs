5b2295aa5a4b76706204ce237056100a06a890ae
Jonathan R Holman <Theheadfool@gmail.com>
2026-08-01 08:58:33 -0700

    Read EPUB author metadata
    Former-commit-id: 22e39945135c38c5d14d5897d1d557e922bc0171

--- src/SmartRenamer/Models/E_EbookMetadata.cs
+++ src/SmartRenamer/Models/E_EbookMetadata.cs
@@ -4,23 +4,13 @@
     /// =========================================================================
     /// E_EbookMetadata
     /// =========================================================================
-    ///
-    /// Represents metadata extracted from a single EPUB file.
-    ///
-    /// Future builds will gradually add:
-    ///
-    /// • Author
-    /// • Publisher
-    /// • Language
-    /// • ISBN
-    /// • Series
-    /// • Description
-    /// • Cover
-    ///
+    /// Represents metadata extracted from a single EPUB.
     /// =========================================================================
     /// </summary>
     public sealed class E_EbookMetadata
     {
         public string Title { get; set; } = "";
+
+        public string Author { get; set; } = "";
     }
 }
